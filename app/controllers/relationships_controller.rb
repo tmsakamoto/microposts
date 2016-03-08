edc7c32713210b390c18af4f6479f82eb6028983
tmsakamoto <tmsakamoto@saintmedia.co.jp>
2016-03-08 23:42:00 +0000

    add followings/followers

---
+++ app/controllers/relationships_controller.rb
@@ -0,0 +1,20 @@
+class RelationshipsController < ApplicationController
+  before_action :logged_in_user
+  
+  #create
+  #フォローする他のユーザーのIDをパラメータとして受け取り、
+  #見つかったユーザーを引数としてUserモデルのfollowメソッドを実行
+  def create
+    @user = User.find(params[:followed_id])
+    current_user.follow(@user)
+  end
+  
+  #destroy
+  #現在のユーザーのfollowing_relationshipsを検索して他のユーザーをフォローしている場合は、
+  #そのユーザーを引数としてUserのunfollowメソッドを実行
+  def destroy
+    @user = current_user.following_relationships.find(params[:id]).followed
+    current_user.unfollow(@user)
+  end
+  
+end
