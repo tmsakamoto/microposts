59f950ea675fbbfbf2a5e12d4aa616b65158d464
tmsakamoto <tmsakamoto@saintmedia.co.jp>
2016-03-08 02:23:15 +0000

    add prefecture and url and self_introduction to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,14 @@
 class User < ActiveRecord::Base
     before_save { self.email = email.downcase }
+    
+    belongs_to :prefecture
+    
     validates :name, presence: true, length: { maximum: 50 }
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, presence: true, length: { maximum: 255 },
                       format: { with: VALID_EMAIL_REGEX },
                       uniqueness: { case_sensitive: false }
+    validates :url, format: /\A#{URI::regexp(%w(http https))}\z/, length: { maximum: 255 }
+    validates :self_introduction, length: { maximum: 1000 }
     has_secure_password
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160304021116) do
+ActiveRecord::Schema.define(version: 20160308013523) do
 
-  create_table "users", force: :cascade do |t|
-    t.string   "name"
-    t.string   "email"
-    t.string   "password_digest"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+  create_table "prefectures", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  create_table "users", force: :cascade do |t|
+    t.string   "name",              limit: 255
+    t.string   "email",             limit: 255
+    t.string   "password_digest",   limit: 255
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
+    t.integer  "prefecture_id",     limit: 4
+    t.string   "url",               limit: 255
+    t.text     "self_introduction", limit: 65535
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["prefecture_id"], name: "index_users_on_prefecture_id", using: :btree
 
 end
