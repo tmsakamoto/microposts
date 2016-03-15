9a15132d70e11b7932a22ff28518b056df011113
tmsakamoto <tmsakamoto@saintmedia.co.jp>
2016-03-15 01:56:38 +0000

    add followings and followers

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,6 +23,7 @@ class User < ActiveRecord::Base
   has_many :follower_users, through: :follower_relationships, source: :follower
   
   #他のユーザーをフォローする
+  #other_user.idがカラムfollowed_idの中にあるか探す
   def follow(other_user)
     following_relationships.find_or_create_by(followed_id: other_user.id)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,11 @@ Rails.application.routes.draw do
   post   'login' , to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
   
-  resources :users
+  resources :users do
+    member do
+      get :followings, :followers
+    end
+  end
   resources :microposts
   resources :relationships, only: [:create, :destroy]
   resources :sessions, only: [:new, :create, :destroy]
